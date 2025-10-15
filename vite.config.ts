015dca5d71ab0ef1acfe96083d1dd24765161b32
soonupy97 <153884872+soonupy@users.noreply.github.com>
2025-10-15 22:44:35 +0900

    도메인 테스트 커밋

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/soonupy/",
+  base: "/portfolio/",
   plugins: [react()],
   css: {
     devSourcemap: true, // CSS 소스맵 활성화 (Vite 3+)
